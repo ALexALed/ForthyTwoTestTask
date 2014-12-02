a87151b239547d4e759b8ef798ac42639a7e8618
alexaled <alexaled@gmail.com>
2014-12-02 22:14:58 +0200

    ticket:1 tests fail

--- my_bio/tests.py
+++ my_bio/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.test.client import Client
+from my_bio.models import Bio
 
-# Create your tests here.
+class BioTests(TestCase):
+    def test_bio_in_the_context(self):
+        client = Client()
+        response = client.get('/')
+        self.assertEquals(list(response.context['bio_list']), [])
+        Bio.objects.create(first_name='Oleksii', last_name='bar')
+        response = client.get('/')
+        self.assertEquals(response.context['bio_list'].count(), 1)
