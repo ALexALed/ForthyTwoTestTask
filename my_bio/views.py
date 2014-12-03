2cd40bdb8cef04da8358957c83b7c8b6fee58d45
alexaled <alexaled@gmail.com>
2014-12-03 21:59:47 +0200

    ticket:3 tests done, all done

--- my_bio/views.py
+++ my_bio/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render_to_response
 
 from models import MyBio, HttpRequestSave
 
+
 def bio_home(request):
     bio_list = MyBio.objects.all()
     return render_to_response('bio/bio_index.html', {'bio_list': bio_list})
 
 def requests(request):
-    requests_list = HttpRequestSave.objects.all()
+    requests_list = HttpRequestSave.objects.all()[0:10]
     return render_to_response('bio/requests.html', {'requests_list': requests_list})
