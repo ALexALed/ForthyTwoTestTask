74e7026132836e0dba784e6ac8e89ca170bb6e2c
alexaled <alexaled@gmail.com>
2014-12-06 21:52:46 +0200

    ticket:9 all done

---
+++ my_bio/management/commands/show_models.py
@@ -0,0 +1,22 @@
+# - *- coding: utf- 8 - *-
+__author__ = 'alexaled'
+
+from django.apps import apps
+from django.core.management.base import BaseCommand
+import sys
+
+class Command(BaseCommand):
+
+    help = 'This command print all models and count their elements'
+
+    def handle(self, *args, **options):
+
+        result_list = []
+        for model in apps.get_models():
+            result_list.append('[%s] - %s objects' % (model.__name__,
+                                        model._default_manager.count()))
+        result_list = '\n'.join(result_list)
+        sys.stdout.write(result_list)
+        sys.stderr.write("\nerror: "+ result_list)
+
+        return result_list
